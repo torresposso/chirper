2b3c105be544df07f9bbb8e36c31970f35940beb
torresposso <erictorres.co@gmail.com>
2025-09-18 18:20:46 -0500

    fix stylesheet link tag

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
   <title>{{ isset($title) ? "$title - Chirper" : 'Chirper' }}</title>
 
   <link rel="preconnect" href="<https://fonts.bunny.net>">
-  <link href="<https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700>" rel="stylesheet" />
+  <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
 
   <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
   <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
